94909333354fcb1e5f53ef8bc5dd8a4d7756b463
nontawat18 <non18tatong@gmail.com>
2023-02-14 10:39:53 +0700

    edit dist2

--- components/about/index.vue
+++ components/about/index.vue
@@ -219,7 +219,6 @@ export default {
         },
       ],
       tab: null,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     };
   },
 };
